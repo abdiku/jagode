296630bd48e5cc72d641e6ed9ea79d074f759d06
abdiku <rahomabdi11@gmail.com>
2025-07-22 12:43:19 +0700

    Create JsonLD -sPartOf-WebPage.js

---
+++ JsonLD -sPartOf-WebPage.js
@@ -0,0 +1,46 @@
+<!-- Start  JsonLD isPartOf WebPage  -->
+<b:if cond='data:view.isMultipleItems'>
+<script type='application/ld+json'>
+{&quot;@context&quot;:&quot;https://schema.org&quot;,
+&quot;@graph&quot;:[
+{&quot;@type&quot;:&quot;Organization&quot;,
+&quot;@id&quot;:&quot;<data:blog.canonicalHomepageUrl/>#organization&quot;,
+&quot;name&quot;:&quot;<data:blog.title/>&quot;,
+&quot;url&quot;:&quot;<data:blog.canonicalHomepageUrl/>&quot;,
+&quot;address&quot;: {&quot;@type&quot;: &quot;PostalAddress&quot;,
+&quot;addressLocality&quot;: &quot;Yogyakarta,D.I.Yogyakarta, Indonesia&quot;,
+&quot;streetAddress&quot;: &quot;Jalan Panembahan Mangkurat No. 121b, RT 45 RW 12 Panembahan, Kraton KRATON, KOTA YOGYAKARTA, DI YOGYAKARTA, ID, 55131&quot; },
+&quot;email&quot;: &quot;kratonrahman2@gmail.com&quot;,
+&quot;telephone&quot;: &quot;+628998188966&quot;,
+&quot;sameAs&quot;:[&quot;https://www.facebook.com/username&quot;,
+&quot;https://www.instagram.com/username&quot;,
+&quot;https://www.youtube.com/username&quot;,
+&quot;https://pinterest.com/username&quot;,
+&quot;https://twitter.com/username&quot;],
+&quot;logo&quot;:{&quot;@type&quot;:&quot;ImageObject&quot;,
+&quot;@id&quot;:&quot;<data:blog.canonicalHomepageUrl/>#logo&quot;,
+&quot;url&quot;:&quot;https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhDzK32Ov4jAL13DvHv2r0gg0AkLkPuwW3omxzbubh69FB8QzYBr5vIdzPn6RvRraQ71TP6QzEIfdbB3DxEmHRA4J0j7BQO_8bB6hCLJA5lRfsON0UuFRMb-6XIWC-gwYG84wncBsonWBVU595T8Ky0dnMQO_iDGdzoPI_TxGvpOUB0pvnBsovkmuVPww/s320/1.png&quot;,
+&quot;width&quot;:512,
+&quot;height&quot;:512,
+&quot;caption&quot;:&quot;<data:blog.title/>&quot;},
+&quot;image&quot;:{&quot;@id&quot;:&quot;<data:blog.canonicalHomepageUrl/>#logo&quot;}},
+{&quot;@type&quot;:&quot;WebSite&quot;,
+&quot;@id&quot;:&quot;<data:blog.canonicalHomepageUrl/>#website&quot;,
+&quot;url&quot;:&quot;<data:blog.canonicalHomepageUrl/>&quot;,
+&quot;name&quot;:&quot;<data:blog.title/>&quot;,
+&quot;description&quot;:&quot;<data:blog.metaDescription.escaped/>&quot;,
+&quot;publisher&quot;:{&quot;@id&quot;:&quot;<data:blog.canonicalHomepageUrl/>#organization&quot;},
+&quot;potentialAction&quot;:{&quot;@type&quot;:&quot;SearchAction&quot;,
+&quot;target&quot;:&quot;<data:blog.canonicalHomepageUrl/>search?q={search_term_string}&quot;,
+&quot;query-input&quot;:&quot;required name=search_term_string&quot;}},
+{&quot;@type&quot;:&quot;WebPage&quot;,
+&quot;@id&quot;:&quot;<data:blog.canonicalHomepageUrl/>#webpage&quot;,
+&quot;url&quot;:&quot;<data:blog.canonicalHomepageUrl/>&quot;,
+&quot;inLanguage&quot;:&quot;id-ID&quot;,
+&quot;name&quot;:&quot;<data:blog.title/>&quot;,
+&quot;isPartOf&quot;:{&quot;@id&quot;:&quot;<data:blog.canonicalHomepageUrl/>#website&quot;},
+&quot;about&quot;:{&quot;@id&quot;:&quot;<data:blog.canonicalHomepageUrl/>#organization&quot;},
+&quot;description&quot;:&quot;<data:blog.metaDescription.escaped/>&quot;}]}
+</script>
+</b:if>
+<!--  End  JsonLD isPartOf WebPage  -->
