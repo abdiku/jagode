fa3b127a97ac5f8f9c8372def5e76235d6fe0ca4
abdiku <rahomabdi11@gmail.com>
2025-07-22 08:08:47 +0700

    Create bahasa.js

---
+++ bahasa.js
@@ -0,0 +1,16 @@
+<script>/*<![CDATA[*/
+//oleh www.wendycode.com
+//terakhir diperbarui 22-07-2023
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+            pageLanguage: 'id',
+            includedLanguages: 'en,id,vi,zh-CN,th,ru,ms,ko,ja,it,hi,fr,es,de,ar,tr,jw',
+            layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+        },
+        'google_translate_element'
+    )
+};
+
+function translateOnload(){var script = document.createElement('script');script.async = true;script.src = 'https://cdn.wendycode.com/blogger/widget/google-translate-free.js?cb=googleTranslateElementInit';document.body.appendChild(script);}function wcTjLzy(){translateOnload();localStorage.setItem("wcGoogleTj","true");}var wcTjStrg = localStorage.getItem("wcGoogleTj");if ("true" != wcTjStrg){var t_wcTj = false;var e_wcTj = false;window.addEventListener("scroll",() =>{if ((document.documentElement.scrollTop != 0 && !t_wcTj) || (document.body.scrollTop != 0 && !t_wcTj)){wcTjLzy();t_wcTj = true;e_wcTj = true;}},true);window.addEventListener("click",() =>{if (!e_wcTj && !e_wcTj){wcTjLzy();e_wcTj = true;t_wcTj = true;}},true);}if ("true" === wcTjStrg){translateOnload();}
+/*]]>*/
+</script>
